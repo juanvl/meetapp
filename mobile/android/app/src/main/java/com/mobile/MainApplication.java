37a07ff051de9bd8b87273f33041048778686d8f
Juan <jnvleite@gmail.com>
2019-10-26 18:22:14 -0300

    fix mobile build

--- mobile/android/app/src/main/java/com/mobile/MainApplication.java
+++ mobile/android/app/src/main/java/com/mobile/MainApplication.java
@@ -1,4 +1,4 @@
-package com.meetappmobile;
+package com.mobile;
 
 import android.app.Application;
 import android.content.Context;
--- mobile/ios/mobileTests/mobileTests.m
+++ mobile/ios/mobileTests/mobileTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface meetappmobileTests : XCTestCase
+@interface mobileTests : XCTestCase
 
 @end
 
-@implementation meetappmobileTests
+@implementation mobileTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
