2481a4cdef51039d98377824ed446f6712f122ce
Juan <jnvleite@gmail.com>
2020-02-03 10:24:00 -0300

    express brute

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import Brute from 'express-brute';
+import BruteRedis from 'express-brute-redis';
 import multer from 'multer';
 
 import multerConfig from './config/multer';
@@ -18,8 +20,20 @@ import * as UserOwnedMeetupsValidator from './app/validators/UserOwnedMeetupsVal
 const routes = new Router();
 const upload = multer(multerConfig);
 
+const bruteStore = new BruteRedis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+});
+
+const bruteForce = new Brute(bruteStore);
+
 routes.post('/users', UserValidator.store, UserController.store);
-routes.post('/sessions', SessionValidator.store, SessionController.store);
+routes.post(
+  '/sessions',
+  bruteForce.prevent,
+  SessionValidator.store,
+  SessionController.store
+);
 
 routes.use(authMiddleware);
 
